f00095373ea0a44f6fb728c029adbaaf9ccfa175
holawaleh <holawaleh@gmail.com>
2025-11-01 18:19:22 +0100

    fix: use RANK() to satisfy checker requirement

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,15 +11,14 @@ GROUP BY u.user_id, u.first_name, u.last_name, u.email
 ORDER BY total_bookings DESC;
 
 
--- 2. Window Function: Rank properties using ROW_NUMBER() based on total bookings
--- ROW_NUMBER() assigns unique ranks even in case of ties
+-- 2. Window Function: Rank properties based on total bookings using RANK()
 SELECT 
     p.property_id,
     p.name AS property_name,
     p.city,
     p.price_per_night,
     COUNT(b.booking_id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM property p
 LEFT JOIN booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name, p.city, p.price_per_night
